566d679abce7b80d5c46a92fad0cede270c5a0b7
shn73 <shn-73@mail.ru>
2026-01-05 11:16:22 +0300

    test ci 4

--- users/tests.py
+++ users/tests.py
@@ -16,34 +16,34 @@ class SubscriptionTests(APITestCase):
         )
         self.subscribe_url = reverse("users:subscribe")
 
-    def test_subscribe_to_course(self):
+    # def test_subscribe_to_course(self):
+    #     self.client.force_authenticate(user=self.user)
+    #
+    #     response = self.client.post(self.subscribe_url, {"course_id": self.course.id})
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    #     self.assertEqual(response.data["message"], "подписка добавлена")
+    #
+    #     subscription_exists = Subscription.objects.filter(
+    #         subscription_user=self.user, subscription_course=self.course
+    #     ).exists()
+    #     self.assertTrue(subscription_exists)
+
+    def test_unsubscribe_from_course(self):
         self.client.force_authenticate(user=self.user)
+
+        Subscription.objects.create(
+            subscription_user=self.user, subscription_course=self.course
+        )
 
         response = self.client.post(self.subscribe_url, {"course_id": self.course.id})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["message"], "подписка добавлена")
+        self.assertEqual(response.data["message"], "подписка удалена")
 
         subscription_exists = Subscription.objects.filter(
             subscription_user=self.user, subscription_course=self.course
         ).exists()
-        self.assertTrue(subscription_exists)
+        self.assertFalse(subscription_exists)
 
-#     def test_unsubscribe_from_course(self):
-#         self.client.force_authenticate(user=self.user)
-#
-#         Subscription.objects.create(
-#             subscription_user=self.user, subscription_course=self.course
-#         )
-#
-#         response = self.client.post(self.subscribe_url, {"course_id": self.course.id})
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertEqual(response.data["message"], "подписка удалена")
-#
-#         subscription_exists = Subscription.objects.filter(
-#             subscription_user=self.user, subscription_course=self.course
-#         ).exists()
-#         self.assertFalse(subscription_exists)
-#
 #     def test_subscribe_to_non_existent_course(self):
 #         self.client.force_authenticate(user=self.user)
 #
