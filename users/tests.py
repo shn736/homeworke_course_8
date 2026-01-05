ff8ea5d78e047d4180cc77226ec96dfa07f5668f
shn73 <shn-73@mail.ru>
2026-01-05 11:21:27 +0300

    test ci 4

--- users/tests.py
+++ users/tests.py
@@ -28,24 +28,24 @@ class SubscriptionTests(APITestCase):
     #     ).exists()
     #     self.assertTrue(subscription_exists)
 
-    def test_unsubscribe_from_course(self):
+    # def test_unsubscribe_from_course(self):
+    #     self.client.force_authenticate(user=self.user)
+    #
+    #     Subscription.objects.create(
+    #         subscription_user=self.user, subscription_course=self.course
+    #     )
+    #
+    #     response = self.client.post(self.subscribe_url, {"course_id": self.course.id})
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    #     self.assertEqual(response.data["message"], "подписка удалена")
+    #
+    #     subscription_exists = Subscription.objects.filter(
+    #         subscription_user=self.user, subscription_course=self.course
+    #     ).exists()
+    #     self.assertFalse(subscription_exists)
+
+    def test_subscribe_to_non_existent_course(self):
         self.client.force_authenticate(user=self.user)
 
-        Subscription.objects.create(
-            subscription_user=self.user, subscription_course=self.course
-        )
-
-        response = self.client.post(self.subscribe_url, {"course_id": self.course.id})
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["message"], "подписка удалена")
-
-        subscription_exists = Subscription.objects.filter(
-            subscription_user=self.user, subscription_course=self.course
-        ).exists()
-        self.assertFalse(subscription_exists)
-
-#     def test_subscribe_to_non_existent_course(self):
-#         self.client.force_authenticate(user=self.user)
-#
-#         response = self.client.post(self.subscribe_url, {"course_id": 9999})
-#         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        response = self.client.post(self.subscribe_url, {"course_id": 9999})
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
